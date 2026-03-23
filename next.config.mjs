22c0eead49655619c3a04f42f2b1e4088339fd01
jtominaga <jtominaga@tominaga-fp.com>
2026-03-23 11:07:54 +0900

    ショートリンク追加: /go/fb, /go/x, /go/th

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,21 @@ const nextConfig = {
         destination: '/lp?utm_source=email&utm_medium=dm&utm_campaign=19kai',
         permanent: false,
       },
+      {
+        source: '/go/fb',
+        destination: 'https://sakuseikun.jp/lp?utm_source=facebook&utm_medium=social&utm_campaign=monitor',
+        permanent: false,
+      },
+      {
+        source: '/go/x',
+        destination: 'https://sakuseikun.jp/lp?utm_source=twitter&utm_medium=social&utm_campaign=monitor',
+        permanent: false,
+      },
+      {
+        source: '/go/th',
+        destination: 'https://sakuseikun.jp/lp?utm_source=threads&utm_medium=social&utm_campaign=monitor',
+        permanent: false,
+      },
     ];
   },
 };
